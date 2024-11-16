1cf76f501741e6269dbd1779b0e8301086d830f2
Ojiromayo <labeodanojiromayo@gmail.com>
2024-11-16 13:31:23 +0100

    Add Render domain to ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,12 @@ SECRET_KEY = getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'bananabliss-api.onrender.com',
+]
+
 
 
 # Application definition
