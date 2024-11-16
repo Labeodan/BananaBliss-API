11c06fbeecfede4d0bf194fab551cfbe0d35b10b
Ojiromayo <labeodanojiromayo@gmail.com>
2024-11-16 20:37:13 +0100

    added cors

--- project/settings.py
+++ project/settings.py
@@ -54,9 +54,11 @@ INSTALLED_APPS = [
     'products',
     'carts',
     'orders',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -65,6 +67,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 
 ROOT_URLCONF = 'project.urls'
 
