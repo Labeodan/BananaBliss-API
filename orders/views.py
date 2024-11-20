47a99bd96ff174d0a65b5d48900dc50adf653d74
Ojiromayo <labeodanojiromayo@gmail.com>
2024-11-20 10:23:49 +0100

    edited order view and serializer

--- orders/views.py
+++ orders/views.py
@@ -5,22 +5,15 @@ from rest_framework.permissions import IsAuthenticated
 from utils.permissions import IsAdminOrOwner
 
 class OrderListCreateView(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated, IsAdminOrOwner]  # Using custom permission
+    permission_classes = [IsAuthenticated, IsAdminOrOwner]
     serializer_class = OrderSerializer
 
     def get_queryset(self):
-        # Admin can see all orders, users can only see their own orders
+        # Admin can see all orders; users can only see their own orders
         if self.request.user.role == 'admin':
             return Order.objects.all()
         return Order.objects.filter(user=self.request.user)
 
-    def perform_create(self, serializer):
-        # Automatically associate the logged-in user with the order when it's created
-        if self.request.user.role != 'admin':
-            serializer.save(user=self.request.user)
-        else:
-            # Admin can assign orders without associating with the user
-            serializer.save()
 
 
 class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
